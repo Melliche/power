a6f006ab5585a0817ce2b57c69218f3cb0a994bf
Julick Mellah <j.mellah@icloud.com>
2022-10-22 02:52:35 +0200

    room socket effective et debut generation de board

--- app.js
+++ app.js
@@ -24,12 +24,52 @@ server.listen(port, () => {
     console.log(`Listening on http://localhost:${port}/`);
 });
 
+let rooms = [];
+
 io.on('connection', (socket) => {
     console.log('a user connected');
 
-    socket.on('play', () => {
+    socket.on('name', (player) => {
+        console.log(player + 'name')
+    })
+
+    socket.on('playerData', (player) => {
+
+        let room = null;
+
+        if (!player.roomId) {
+            room = createRoom(player);
+            console.log(player)
+            console.log(`[create room ] - ${room.id} - ${player.username}`);
+        } else {
+            room = rooms.find(r => r.id === player.roomId);
+
+            if (room === undefined) {
+                console.log('mince')
+                return;
+            }
+            console.log('ok')
+            player.roomId = room.id;
+            room.players.push(player);
+        }
+
+        socket.join(room.id);
+
+        io.to(socket.id).emit('join room', room.id);
+
+        if (room.players.length === 2) {
+            io.to(room.id).emit('start game', room.players);
+        }
+    });
+
+    socket.on('get rooms', () => {
+        io.to(socket.id).emit('list rooms', rooms);
+    });
+
+    socket.on('play', (roomId) => {
         console.log('message');
-        io.emit('play')
+        io.to(roomId).emit('play');
+        // io.emit('play')
       });
 
     socket.on('disconnect', () => {
@@ -37,6 +77,19 @@ io.on('connection', (socket) => {
       });
 });
 
+function createRoom(player) {
+    const room = { id: roomId(), players: [] };
 
+    player.roomId = room.id;
+    console.log('create room')
+    room.players.push(player);
+    rooms.push(room);
+    console.log(player)
+    return room;
+}
 
-console.log('youou')
+//génère un id random de salon
+function roomId() {
+    return Math.random().toString(36).substr(2, 9);
+}
+
--- public/js/index.js
+++ public/js/index.js
@@ -8,19 +8,96 @@ const player = {
     socketId: "",
 };
 
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const boite = document.getElementById('box');
 
-boite = document.getElementById('box');
+const check = document.getElementById('check');
 
-boite.addEventListener("click", function(e) {
-    // e.preventDefault();
-    console.log('clickonboite')    
-    socket.emit('play', );
+const roomsCard = document.getElementById('rooms-card');
+const roomsList = document.getElementById('rooms-list');
+
+socket.emit('get rooms');
+
+socket.on('list rooms', (rooms) => {
+    html = '';
+    if (rooms.length > 0) {
+        console.log('rooms')
+        rooms.forEach(room => {
+            if (room.players.length !== 2) {
+                html += `<li class="room">
+                <p class="p-room">Salon de ${room.players[0].username} - ${room.id}</p>
+                <button class="join-room" data-room="${room.id}">Rejoindre</button>
+             </li>`;
+            }
+        });
+    }
+    if (html !== "") {
+        console.log('roomzs')
+        roomsCard.style.display = 'contents';
+        roomsList.innerHTML = html;
+
+        for (const element of document.getElementsByClassName('join-room')) {
+            element.addEventListener('click', joinRoom, false)
+        }
+    }
+})
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    player.username = input.value;
+    player.socketId = socket.id;
+    player.host = true;
+
+    roomsCard.style.display = 'none';
+    roomsList.style.display = 'none';
+
+    socket.emit('playerData', player);
+
 });
 
+boite.addEventListener("click", function(e) {
+    console.log('clickonboite')    
+    socket.emit('play', player.roomId);
+});
+
+check.addEventListener("click", function(e) {
+    console.log(player)
+    table();
+});
 
 socket.on('play', () => { 
-    console.log('efefe')
     boite.style.display = 'none';
 });
 
-// console.log('index')
+socket.on('join room', (roomId) => {
+    player.roomId = roomId;
+});
+
+
+// Quand un joueur rejoins une room et qu'il n'est pas l'host
+const joinRoom = function () {
+    console.log('joinroom')
+    if (input.value !== '' && input.value.length >= 3) {
+        player.username = input.value;
+        player.socketId = socket.id;
+        player.roomId = this.dataset.room;
+
+        socket.emit('playerData', player);
+
+        // userCard.hidden = true;
+        // waitingArea.style.display = 'contents';
+        roomsCard.style.display = 'none';
+        roomsList.style.display = 'none';
+    }
+}
+
+function table(rows = 6, columns = 7) {
+    board = [];
+    for(let i = 0; i < rows; i++){
+        board[i] = Array(columns).fill(0);
+    }
+    console.log(board)
+
+}
